caa9b4acc142358aa8d79a9ebbfaea280ad08994
Fernando <figarcia3@uc.cl>
2023-11-12 17:22:21 -0300

    first endpoints for search engine

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 
 import json
 
@@ -19,17 +18,41 @@ def hello_world():
     return "<h1>Starter Flask App</h1>"
 
 
-@app.route("/create-index", methods=['POST'])
-def create_index():
-    data = request.get_json()
-    app.logger.info(data)
-    return {}
+@app.route("/index", methods=['POST', 'GET'])
+def list_index():
+    if request.method == 'GET':
+        return jsonify(es.indices.get_alias("*")), 200
+    if request.method == 'POST':
+        body = request.get_json()
+        es.indices.create(index=body["name"], mappings=body["mappings"])
+        return jsonify(body), 201
+    return jsonify(), 400
 
 
-@app.route("/set-config", methods=['POST'])
-def set_config():
-    request.form
-    return {}
+@app.route("/index/<string:index_name>", methods=['GET', 'DELETE'])
+def show_index(index_name):
+    if request.method == 'DELETE':
+        es.indices.delete(index=index_name)
+        return jsonify(), 204
+    if request.method == 'GET':
+        result = es.indices.get_alias(index=index_name)
+        return jsonify(result.body), 200
+    return jsonify(), 400
+
+
+@app.route("/index/<string:index_name>/document/<string:id>", methods=['POST', 'GET', 'DELETE'])
+def document_index(index_name, id):
+    if request.method == 'POST':
+        body = request.get_json()
+        es.index(index=index_name, id=id, document=body)
+        return jsonify(body), 200
+    if request.method == 'DELETE':
+        es.delete(index=index_name, id=id)
+        return jsonify(), 204
+    if request.method == 'GET':
+        response = es.get(index=index_name, id=id)
+        return jsonify(response.body), 200
+    return jsonify(), 400
 
 
 if __name__ == '__main__':
