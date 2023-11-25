8d1af439701b78640c2cd58e8f8378d0cc2fab97
Fernando <figarcia3@uc.cl>
2023-11-25 20:54:45 -0300

    fix

--- search/urls.py
+++ search/urls.py
@@ -1,9 +1,18 @@
 from django.urls import re_path
 
 
-from .views import ListIndexView, ShowIndexView
+from .views import AddDocumentsView, ListIndexView, SearchView, ShowIndexView, DocumentShowView, DocumentIndexView
 
 urlpatterns = [
     re_path(r"^index/$", ListIndexView.as_view()),
+    re_path(
+        r"^index/(?P<index_name>[\w-]+)/add-documents/$", AddDocumentsView.as_view()),
     re_path(r"^index/(?P<index_name>[\w-]+)/$", ShowIndexView.as_view()),
+    re_path(r"^index/(?P<index_name>[\w-]+)/search/$",
+            SearchView.as_view()),
+    re_path(
+        r"^index/(?P<index_name>[\w-]+)/document/$", DocumentIndexView.as_view()),
+    re_path(
+        r"^index/(?P<index_name>[\w-]+)/document/(?P<id>[\w-]+)/$", DocumentShowView.as_view()),
+
 ]
--- search/views.py
+++ search/views.py
@@ -1,3 +1,4 @@
+import json
 from django.http import JsonResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
@@ -5,6 +6,7 @@ from django.utils.decorators import method_decorator
 
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
+from search.utils import transform_json_list
 
 # Assuming you have a Django project with an app named 'your_app'
 # Add this to your_app/views.py
@@ -16,20 +18,20 @@ es = Elasticsearch("elastic://search:9200")
 class ListIndexView(View):
     def get(self, request):
         result = es.indices.get_alias(index="*")
-        return JsonResponse(result, safe=False, status=200)
+        return JsonResponse(result.body, safe=False, status=200)
 
     def post(self, request):
-        body = self.request.json
-        es.indices.create(
+        body = json.loads(request.body)
+        result = es.indices.create(
             index=body["name"], mappings=body["mappings"], settings=body["settings"])
-        return JsonResponse(body, safe=False, status=201)
+        return JsonResponse(result.body, safe=False, status=201)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ShowIndexView(View):
     def get(self, request, index_name):
         result = es.indices.get_alias(index=index_name)
-        return JsonResponse(result, safe=False, status=200)
+        return JsonResponse(result.body, safe=False, status=200)
 
     def delete(self, request, index_name):
         es.indices.delete(index=index_name)
@@ -38,6 +40,19 @@ class ShowIndexView(View):
 
 @method_decorator(csrf_exempt, name='dispatch')
 class DocumentIndexView(View):
+    def get(self, request, index_name):
+        response = es.search(index=index_name, body={
+                             "query": {"match_all": {}}})
+        documents = []
+        for hit in response["hits"]["hits"]:
+            doc = hit["_source"]
+            doc["id"] = hit["_id"]
+            documents.append(doc)
+        return JsonResponse(documents, safe=False, status=200)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DocumentShowView(View):
     def get(self, request, index_name, id):
         response = es.get(index=index_name, id=id)
         return JsonResponse(response, safe=False, status=200)
@@ -57,7 +72,7 @@ class SearchView(View):
     def post(self, request, index_name):
         query = self.request.json
         response = es.search(index=index_name, body=query)
-        return JsonResponse(response, safe=False, status=200)
+        return JsonResponse(response.body, safe=False, status=200)
 
     def get(self, request, index_name):
         search_term = self.request.GET.get('q')
@@ -80,8 +95,8 @@ class SearchView(View):
                     "functions": [
                         {"filter": {"match": {"class": "W"}}, "weight": 1.1},
                         {"filter": {"match": {"class": "V"}}, "weight": 1.1},
-                        {"filter": {"match": {"category": "U"}}, "weight": 1},
-                        {"filter": {"match": {"category": "C"}}, "weight": 1}
+                        {"filter": {"match": {"class": "U"}}, "weight": 1},
+                        {"filter": {"match": {"class": "C"}}, "weight": 1}
                     ],
                     "score_mode": "avg",
                     "boost_mode": "multiply"
@@ -89,12 +104,17 @@ class SearchView(View):
             }
         }
         response = es.search(index=index_name, body=query)
-        return JsonResponse(response, safe=False, status=200)
+        return JsonResponse(response.body, safe=False, status=200)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class AddDocumentsView(View):
-    def post(self, request):
-        data = self.request.json
-        response = bulk(es, data["documents"])
+    def post(self, request, index_name):
+        mappings = es.indices.get_mapping(index=index_name)
+        mappings_list = mappings[index_name]['mappings']['properties'].keys()
+
+        body = json.loads(request.body)
+        body = transform_json_list(body, index_name, mappings_list)
+
+        response = bulk(es, body)
         return JsonResponse(response, safe=False, status=200)
