4994350d7f5831e512234ebca37dfc26a59de3da
Fernando <figarcia3@uc.cl>
2023-11-26 16:50:22 -0300

    charge synonyms

---
+++ search/utils.py
@@ -0,0 +1,17 @@
+def transform_json_list(json_list, index_name, mappings):
+    transformed_list = []
+
+    for input_json in json_list:
+        source_data = {}
+        for field in mappings:
+            source_data[field] = input_json.get(field, None)
+
+        transformed_json = {
+            "_index": index_name,
+            "_id": input_json["id"],
+            "_source": source_data
+        }
+
+        transformed_list.append(transformed_json)
+
+    return transformed_list
