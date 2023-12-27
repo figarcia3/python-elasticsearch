a09ebe1b128a9c2372511dea2da1e5019d6a5480
figarcia3 <figarcia3@uc.cl>
2023-12-27 00:38:35 -0300

    meesage

--- search/views.py
+++ search/views.py
@@ -421,7 +421,7 @@ class MultiSearchView(View):
         try:
             response_store = es.search(index='store_products', body=query_stores)
         except Exception as e:
-            print("errors ", e)
+            print("errors ", e.message)
         documents_store_products = []
         for hit in response_store["hits"]["hits"]:
             documents_store_products.append(hit["_source"])
